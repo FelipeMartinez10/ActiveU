5d24556371299979f3ff584e01e3bf5efe720827
Nicolás Gómez <n.gomez22@uniandes.edu.co>
2017-05-17 15:08:26 -0500

    Create App component

---
+++ imports/ui/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'; // eslint-disable-line no-unused-vars
+
+
+// App component - represents the whole app
+export default class App extends Component {
+
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>ActiveU</h1>
+        </header>
+      </div>
+    );
+  }
+}
