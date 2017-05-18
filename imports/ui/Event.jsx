1b924bf69abbab09ad12e350f2a09f2f4d1e7e36
Felipe <f.martinez11@uniandes.edu.co>
2017-05-18 18:56:20 -0500

    pagination

--- imports/ui/Event.jsx
+++ imports/ui/Event.jsx
@@ -1,7 +1,34 @@
 import React, { Component, PropTypes } from 'react'; // eslint-disable-line no-unused-vars
-
+import {Pagination} from 'react-bootstrap';
 export default class Event extends Component {
+  constructor() {
+   super();
+   this.state = {
+     events: ['a','b','c','d','e','f','g','h','i','j','k'],
+     currentPage: 1,
+     eventsPerPage: 4
+   };
+   this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(event) {
+   this.setState({
+     currentPage: Number(event)
+   });
+  }
+
   render() {
+    const { events, currentPage, eventsPerPage } = this.state;
+    const indexOfLastEvent = currentPage * eventsPerPage;
+    const indexOfFirstEvent = indexOfLastEvent - eventsPerPage;
+    const currentEvents = events.slice(indexOfFirstEvent, indexOfLastEvent);
+    const renderEvents = currentEvents.map((event, index) => {
+      return <li key={index}>{event}</li>;
+    });
+
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(events.length / eventsPerPage); i++) {
+      pageNumbers.push(i);
+    }
     return (
       <div className='container-fluid'>
         <div className="row">
@@ -15,8 +42,16 @@ export default class Event extends Component {
             </div>
           </div>
           <div className='col-md-6'>
-            <div className="box">
-            	<h1>Hola</h1>
+            <div className="box events">
+            	<h1>Eventos:</h1>
+              <ul>
+                {renderEvents}
+              </ul>
+              <Pagination
+               bsSize="medium"
+               items={pageNumbers.length}
+               activePage={this.state.currentPage}
+               onSelect={this.handleClick} />
             </div>
           </div>
           <div className='col-md-1'></div>
