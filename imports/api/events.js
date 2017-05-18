955e8af495b9e43585fd088c8ed21fa92fb6307b
Nicolás Gómez <n.gomez22@uniandes.edu.co>
2017-05-17 20:22:31 -0500

    Add method to create an event

--- imports/api/events.js
+++ imports/api/events.js
@@ -1,3 +1,43 @@
 import { Mongo } from 'meteor/mongo';
+import { ValidatedMethod } from 'meteor/mdg:validated-method';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 
 export const Events = new Mongo.Collection('events');
+
+Events.deny({
+  insert() {
+    return true;
+  },
+  update() {
+    return true;
+  },
+  remove() {
+    return true;
+  }
+});
+
+export const createEvent = new ValidatedMethod({
+  name: 'events.create',
+  validate: new SimpleSchema({
+    name: { type: String },
+    place: { type: String },
+    type: { type: String },
+    description: { type: String },
+    when: { type: Date },
+    howMany: { type: Number }
+  }).validator(),
+  run({ name, place, type, description, when, howMany }) {
+    if (!this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+    return Events.insert({
+      owner: this.userId,
+      name,
+      place,
+      type,
+      description,
+      when,
+      howMany
+    });
+  }
+});
