6e5f7dac79a14113fe39c963176ce2e71f87b84e
Nicolás Gómez <n.gomez22@uniandes.edu.co>
2017-05-23 12:27:04 -0500

    no-unused vars in Detalles component

--- imports/ui/Detalles.jsx
+++ imports/ui/Detalles.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable no-unused-vars */
 import React, {Component} from "react";
 import { Meteor } from "meteor/meteor";
-import {Button} from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+/* eslint-enable no-unused-vars */
 
 export default class Detalles extends Component {
   render() {
