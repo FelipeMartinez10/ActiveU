b92a201aee5bc8d13afed0284c4dfd04abcc1a26
Nicolás Gómez <n.gomez22@uniandes.edu.co>
2017-05-23 16:35:21 -0500

    Create chat component

--- imports/ui/Chat.jsx
+++ imports/ui/Chat.jsx
@@ -2,4 +2,57 @@
 import React, {Component} from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Button } from 'react-bootstrap';
+import '../style/Chat.css';
 /* eslint-enable no-unused-vars */
+
+export default class Chat extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hidden: true
+    };
+  }
+
+  toggleHidden() {
+    this.setState({
+      hidden: !this.state.hidden
+    });
+  }
+
+  render() {
+    return this.state.hidden ? (
+      <div className="panel panel-primary">
+        <div className="panel-heading" id="accordion">
+          <span className="fa fa-comments"></span> Chat
+          <span
+            className="pull-right fa fa-caret-down"
+            onClick={ () => { this.toggleHidden(); } }
+          />
+        </div>
+      </div>
+    ) : (
+      <div className="panel panel-primary">
+        <div className="panel-heading" id="accordion">
+          <span className="fa fa-comments"></span> Chat
+          <span
+            className="pull-right fa fa-caret-up"
+            onClick={ () => { this.toggleHidden(); } }
+          />
+        </div>
+        <div className="panel-body">
+          <p>mensajes mensajes</p>
+        </div>
+        <div class="panel-footer">
+          <div class="input-group">
+            <input id="btn-input" type="text" class="form-control input-sm" placeholder="Type your message here..." />
+            <span class="input-group-btn">
+              <button class="btn btn-warning btn-sm">
+                Send
+              </button>
+            </span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
--- imports/ui/Detalles.jsx
+++ imports/ui/Detalles.jsx
@@ -2,39 +2,44 @@
 import React, {Component} from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Button } from 'react-bootstrap';
+import Chat from './Chat.jsx';
 /* eslint-enable no-unused-vars */
 
 export default class Detalles extends Component {
   render() {
     return (
-      <div className="card hovercard">
-        <div className="cardheader"></div>
-        <div className="avatar">
-          <img alt="" src="img/avatar/hombre1.png"></img>
-        </div>
-        <div className='container-fluid'>
-          <div className='row'>
-            <h4>{this.props.name}</h4>
+      <div>
+        <div className="card hovercard">
+          <div className="cardheader"></div>
+          <div className="avatar">
+            <img alt="" src="img/avatar/hombre1.png"></img>
           </div>
-          <div className='row'>
-            <h5>Descripción:</h5>
-          </div>
-          <div className='row details-description'>
-            <p>{this.props.description}</p>
-          </div>
-          {/* Depronto es redundante esta info.
+          <div className='container-fluid'>
             <div className='row'>
-            <p>{this.props.type}</p>
-            <p>{this.props.place}</p>
-            <p>{this.props.day}</p>
-            <p>{this.props.month}</p>
-            <p>{this.props.howMany}</p>
-          </div>*/}
-          <div className='row details-button'>
-            <Button bsStyle="success">¡Me interesa! <i className="fa fa-check-circle" aria-hidden="true"></i>
-            </Button>
+              <h4>{this.props.name}</h4>
+            </div>
+            <div className='row'>
+              <h5>Descripción:</h5>
+            </div>
+            <div className='row details-description'>
+              <p>{this.props.description}</p>
+            </div>
+            {/* Depronto es redundante esta info.
+              <div className='row'>
+              <p>{this.props.type}</p>
+              <p>{this.props.place}</p>
+              <p>{this.props.day}</p>
+              <p>{this.props.month}</p>
+              <p>{this.props.howMany}</p>
+            </div>*/}
+            <div className='row details-button'>
+              <Button bsStyle="success">¡Me interesa! <i className="fa fa-check-circle" aria-hidden="true"></i>
+              </Button>
+            </div>
           </div>
         </div>
-    </div>);
+        <Chat />
+      </div>
+    );
   }
 }
