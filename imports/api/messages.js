979862b168342f474cf0ef7ba08db21e4b7c1b5b
Nicolás Gómez <n.gomez22@uniandes.edu.co>
2017-05-23 14:48:45 -0500

    Add messages collection

---
+++ imports/api/messages.js
@@ -0,0 +1,56 @@
+import { Mongo } from 'meteor/mongo';
+import { ValidatedMethod } from 'meteor/mdg:validated-method';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+
+export const Messages = new Mongo.Collection('messages');
+
+Messages.deny({
+  insert() {
+    return true;
+  },
+  update() {
+    return true;
+  },
+  remove() {
+    return true;
+  }
+});
+
+export const newMessage = new ValidatedMethod({
+  name: 'messages.new',
+  validate: new SimpleSchema({
+    event: { type: Number },
+    text: { type: String },
+  }).validator(),
+  run({ event, text }) {
+    if (!this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+    return Messages.insert({
+      event,
+      user: this.userId,
+      time: new Date(),
+      text
+    });
+  }
+});
+
+export const removeMessage = new ValidatedMethod({
+  name: 'messages.remove',
+  validate: new SimpleSchema({
+    id: { type: Number }
+  }).validator(),
+  run({ id }) {
+    if (!this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+    Messages.remove(id);
+  }
+});
+
+if (Meteor.isServer) {
+  // This code only runs on the server
+  Meteor.publish('messages', function msgsPublication() {
+    return Messages.find({});
+  });
+}
