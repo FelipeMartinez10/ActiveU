496a8e4e5cc7d403ba7bbcc4c8cf5b095dc6a3bf
Felipe <f.martinez11@uniandes.edu.co>
2017-05-24 18:27:55 -0500

    create event modal

--- imports/ui/ProfileList.jsx
+++ imports/ui/ProfileList.jsx
@@ -1,31 +1,39 @@
 /* eslint-disable no-unused-vars */
 import React, { Component } from 'react';
-import {ListGroup, ListGroupItem} from 'react-bootstrap/lib/';
+import {ListGroup, ListGroupItem, Modal, Button, ControlLabel, FormGroup, FormControl} from 'react-bootstrap/lib/';
 /* eslint-enable no-unused-vars */
 
 class ProfileList extends Component {
   constructor(props)
   {
     super(props)
+    this.state=
+    {
+      showModal:false
+    }
     this.renderOwnEvents = this.renderOwnEvents.bind(this);
     this.renderOthersEvents = this.renderOthersEvents.bind(this);
-    this.createEvent = this.createEvent.bind(this);
-  }
-  createEvent()
-  {
-    window.alert("holaaaa");
+    this.open = this.open.bind(this);
+    this.close = this.close.bind(this);
   }
   renderOwnEvents()
   {
     console.log("holaaa");
     return(
-    <ListGroupItem onClick={this.createEvent}>
+    <ListGroupItem onClick={this.open}>
       <i className="glyphicon glyphicon-plus"></i> Crea un Evento ya!
     </ListGroupItem>);
   }
   renderOthersEvents()
   {
     console.log("holaaa");
+  }
+  close() {
+    this.setState({ showModal: false });
+  }
+
+  open() {
+    this.setState({ showModal: true });
   }
   render() {
     const own = (this.props.currentList=="own");
@@ -39,7 +47,7 @@ class ProfileList extends Component {
             {(ownNotEmpty)?
               this.renderOwnEvents
               :
-              <ListGroupItem onClick={this.createEvent}>
+              <ListGroupItem onClick={this.open}>
                 <i className="glyphicon glyphicon-plus"></i> Crea un Evento ya!
               </ListGroupItem>}
           </ListGroup>
@@ -52,6 +60,66 @@ class ProfileList extends Component {
                 No te has interesado en ningún evento.
               </ListGroupItem>}
           </ListGroup>}
+          <Modal show={this.state.showModal} onHide={this.close}>
+            <Modal.Header closeButton>
+              <Modal.Title>Crear Evento</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <h4>Ingresa los datos del evento</h4>
+              <div className="form-group">
+                <div className="input-group">
+                  <span className="input-group-addon">
+                    <i className="glyphicon glyphicon-star"></i>
+                  </span>
+                  <input className="form-control" placeholder="Nombre del Evento" type='text' name="nombre"
+                    value={this.state.value}></input>
+                </div>
+              </div>
+              <div className="form-group">
+                <div className="input-group">
+                  <span className="input-group-addon">
+                    <i className="glyphicon glyphicon-map-marker"></i>
+                  </span>
+                  <input className="form-control" placeholder="Lugar" type='text' name="lugar"
+                    value={this.state.value}></input>
+                </div>
+              </div>
+              <div className="form-group">
+                <div className="input-group">
+                  <span className="input-group-addon">
+                    <i className="glyphicon glyphicon-sunglasses"></i>
+                  </span>
+                  <input className="form-control" placeholder="Categoria del evento" type='text' name="tipo"
+                    value={this.state.value}></input>
+                </div>
+              </div>
+              <div className="form-group">
+                <div className="input-group">
+                  <span className="input-group-addon">
+                    <i className="glyphicon glyphicon-user"></i>
+                  </span>
+                  <input className="form-control" placeholder="Cupos disponibles" type='number' name="cupos"
+                    value={this.state.value}></input>
+                </div>
+              </div>
+              <div className="form-group">
+                <div className="input-group">
+                  <span className="input-group-addon">
+                    <i className="glyphicon glyphicon-calendar"></i>
+                  </span>
+                  <input className="form-control" placeholder="Fecha del Evento" type='date' name="fecha"
+                    value={this.state.value}></input>
+                </div>
+              </div>
+              <FormGroup controlId="formControlsTextarea">
+                <ControlLabel>Descripción:</ControlLabel>
+                <FormControl componentClass="textarea" placeholder="Ingresa aquí la descripción." />
+              </FormGroup>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={this.close}>Cerrar</Button>
+            </Modal.Footer>
+        </Modal>
 
       </div>
     );
